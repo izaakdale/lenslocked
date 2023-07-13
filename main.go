6fdb992488cab27058772e4dcf8ba87041e8dc8d
Izaak Dale <izaakdaledev@gmail.com>
2023-07-13 20:34:09 +0100

    Initial commit

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome!</h1>")
 }
