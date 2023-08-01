f2d41b92068841948391c076d04fdaed493ec30c
izaakdale <izaakdaledev@gmail.com>
2023-08-01 23:38:53 +0100

    created not found handler and router struct

--- main.go
+++ main.go
@@ -5,17 +5,33 @@ import (
 	"net/http"
 )
 
+type Router struct {
+}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		notFoundHandler(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe("localhost:3000", nil)
+	http.ListenAndServe("localhost:3000", Router{})
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome!</h1>")
+	fmt.Fprint(w, "<h1>Welcomen!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact</h1><p>To get in touch email me at <a href=\"mailto:izaakdale@live.com\">izaakdale@live.com</a></p>")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Page not found", http.StatusNotFound)
 }
