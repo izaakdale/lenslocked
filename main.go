fbb67c1299ef291596a453c5a1993a943c6797cf
Izaak Dale <izaakdaledev@gmail.com>
2023-08-23 11:44:11 -0700

    added controller package and static handler

--- main.go
+++ main.go
@@ -2,55 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/izaakdale/lenslocked/controllers"
 	"github.com/izaakdale/lenslocked/views"
 )
 
 func main() {
-	mux := chi.NewRouter()
 
+	mux := chi.NewRouter()
 	mux.Use(middleware.Logger)
 
-	mux.Get("/", homeHandler)
-	mux.Get("/contact", contactHandler)
-	mux.Get("/faq", faqHandler)
-	mux.Get("/galleries/{id}", galleriesHandler)
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	mux.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	mux.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	mux.Get("/faq", controllers.StaticHandler(tpl))
+
 	mux.NotFound(notFoundHandler)
 
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe("localhost:3000", mux)
-}
-
-func executeTemplate(w http.ResponseWriter, path string) {
-	tpl, err := views.Parse(path)
-	if err != nil {
-		log.Printf("error parsing template: %v", err)
-		http.Error(w, "template parse error", http.StatusInternalServerError)
-		return
-	}
-
-	tpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "home.gohtml"))
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "contact.gohtml"))
-}
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
-}
-
-func galleriesHandler(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	fmt.Fprintf(w, "<h1>your id is: %s</h1", id)
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
